a8ca0233793e9d2c854bc604aee914a3e57ab48a
racoonman <>
2023-07-10 14:25:09 -0700

    commit

--- src/main/java/com/terraforged/mod/level/levelgen/biome/parameter/BiomeRule.java
+++
@@ -1,5 +0,0 @@
-package com.terraforged.mod.level.levelgen.biome.parameter;
-
-public interface BiomeRule<T> {
-	float fitness(T config, float temperature, float moisture, float continent, float river);
-}
