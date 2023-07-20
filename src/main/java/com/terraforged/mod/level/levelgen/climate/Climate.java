34035d3e3cf438cd23597dd2752f508323c60d95
racoonman <>
2023-07-20 10:59:05 -0700

    changed climate codec for 1.19.2 compat

--- src/main/java/com/terraforged/mod/level/levelgen/climate/Climate.java
+++ src/main/java/com/terraforged/mod/level/levelgen/climate/Climate.java
@@ -1,6 +1,7 @@
 package com.terraforged.mod.level.levelgen.climate;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.terraforged.mod.registry.data.TFClimates;
 import com.terraforged.mod.util.storage.WeightMap;
 
@@ -9,6 +10,8 @@ import net.minecraft.resources.RegistryFileCodec;
 import net.minecraft.world.level.biome.Biome;
 
 public record Climate(WeightMap<Holder<Biome>> biomes) {
-	public static final Codec<Climate> DIRECT_CODEC = WeightMap.codec(Biome.CODEC).xmap(Climate::new, Climate::biomes);
+	public static final Codec<Climate> DIRECT_CODEC = RecordCodecBuilder.create(instance -> instance.group(
+		WeightMap.codec(Biome.CODEC).fieldOf("biomes").forGetter(Climate::biomes)
+	).apply(instance, Climate::new));
 	public static final Codec<Holder<Climate>> CODEC = RegistryFileCodec.create(TFClimates.REGISTRY, DIRECT_CODEC);
 }
